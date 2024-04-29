2e44038ed87ed113d775d7ba8718cbc6ae11bad2
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 14:01:24 -0500

    Add snakeCase support for handlebars varname as {{snakeCase varname}}

--- lib/modules/utils/toVariableName.ts
+++ lib/modules/utils/toVariableName.ts
@@ -1,6 +1,6 @@
 //* Imports
 
-export default function toVariableName (name: string, { capitalize = false, constCase = false } = {}) {
+export default function toVariableName (name: string, { capitalize = false, constCase = false, snakeCase = false } = {}) {
     if (constCase) {
         if (name.includes("-")) {
             name = name.split("-").join("_");
@@ -14,6 +14,10 @@ export default function toVariableName (name: string, { capitalize = false, cons
             return char;
         }).join("").toUpperCase();
 
+    } else if (snakeCase) {
+        name = name.replace(/-/g, '_'); // Replace hyphens with underscores
+        name = name.replace(/[A-Z]/g, match => `_${match.toLowerCase()}`); // Add underscore before uppercase letters
+        name = name.toLowerCase(); // Convert to lowercase
     } else {
         if (name.includes("-")) {
             name = name.split("-").map((part, i) => i === 0 ? part : part[0].toUpperCase() + part.slice(1)).join("");
